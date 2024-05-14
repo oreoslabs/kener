101cbe79ff316ba6c06991302c4045a547a3b5ca
lovedret <lovedret@6block.com>
2024-05-15 00:15:40 +0800

    Update logo to oreowallet logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 {/if}
 <svelte:head>
 	<title>{data.site.title}</title>
-	<link rel="icon" id="kener-app-favicon" href="{base}/logo96.png" />
+	<link rel="icon" id="kener-app-favicon" href="{base}/logo.png" />
 	{#each Object.entries(data.site.metaTags) as [key, value]}
 		<meta name={key} content={value} />
 	{/each}
